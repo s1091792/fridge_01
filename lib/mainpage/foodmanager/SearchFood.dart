ca37d9cd514159e7dc49ae40181647541ec448e0
s1091792 <s10917922@gm.pu.edu.tw>
2023-10-29 17:37:09 +0800

    食材管理：搜尋食材串接好，差刷新 食譜搜尋：搜尋食譜串接好，差刷新

---
+++ lib/mainpage/foodmanager/SearchFood.dart
@@ -0,0 +1,36 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'dart:core';
+import 'package:intl/intl.dart';
+
+List<Map<String, dynamic>> SfoodData = [];
+
+Future<Map<String, dynamic>> SearchFood(String foodName) async {
+  print("進入搜尋食材2");
+  final collection = FirebaseFirestore.instance.collection('food');
+  final querySnapshot = await collection
+      .where('food_name', isEqualTo: foodName)
+      .get();
+
+  if (querySnapshot.docs.isNotEmpty) {
+    print("進入搜尋食材3");
+
+    final document = querySnapshot.docs[0].data();
+
+    print(document);
+    Timestamp timestamp = document['EXP'] as Timestamp;
+    DateTime expDate = timestamp.toDate();
+    String formattedDate = DateFormat('yyyy-MM-dd').format(expDate);
+
+    return {
+      'title': document['food_name'] as String,
+      'date':  formattedDate,
+      'number': document['amount'] as int,
+      'image': document['image'] as String,
+    };
+
+  } else {
+    print("進入搜尋食譜no");
+    return {};
+  }
+}
